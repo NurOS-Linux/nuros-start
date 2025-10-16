87c592118f33526a248b0592f013c05cc21eb3a2
ruzen42 <malledeferlive@gmail.com>
2025-10-16 22:40:14 +0500

    feat(add Formatter.hs)

--- Installer/src/Storage/Filesystem.hs
+++ Installer/src/Storage/Filesystem.hs
@@ -1,17 +1,17 @@
 -- NurOS Ruzen42 2025
 {-# LANGUAGE DeriveGeneric #-}
 
-module Storage.Filesystem where
+module Storage.Filesystem (parseProcPartitions, Disk(..), Partition(..)) where
 
 import Data.Char (isDigit)
 import Data.Text.Short (ShortText)
 import qualified Data.Text.Short as TS
+import qualified Data.Text as T
 import qualified Data.Text.Read as TR
 import qualified Data.Vector as V
 import Data.List (groupBy, sortOn)
 import GHC.Generics (Generic)
 
--- === Types ===
 data Partition = Partition
   { deviceName :: !ShortText
   , number     :: !Int
@@ -22,44 +22,46 @@ data Disk = Disk
   , partitions :: !(V.Vector Partition)
   } deriving (Show, Eq, Generic)
 
--- === Main func ===
-
-parseProcPartitions :: TS.ShortText -> [Disk]
+parseProcPartitions :: ShortText -> [Disk]
 parseProcPartitions input =
   let
-    ls = drop 1 $ TS.lines input  
+    ls = drop 1 $ T.lines (TS.toText input)
     entries = mapMaybe parseLine ls
     grouped = groupBy sameDisk $ sortOn fst entries
   in
-    [ Disk name (V.fromList parts)
+    [ Disk (TS.fromText name) (V.fromList parts)
     | (name, parts) <- map collect grouped
     ]
 
--- === Helping funcs ===
-
-parseLine :: ShortText -> Maybe (ShortText, Partition)
+parseLine :: T.Text -> Maybe (T.Text, Partition)
 parseLine line =
-  case TS.words line of
+  case T.words line of
     (_major : _minor : _blocks : nameTxt : _) ->
-      let name = TS.strip nameTxt
-      in if TS.all isDigit name
+      let name = T.strip nameTxt
+      in if T.all isDigit name
          then Nothing
          else
            let (disk, num) = splitName name
-           in Just (disk, Partition name num)
+           in Just (disk, Partition (TS.fromText name) num)
     _ -> Nothing
 
-splitName :: ShortText -> (ShortText, Int)
+splitName :: T.Text -> (T.Text, Int)
 splitName t =
-  let (prefix, suffix) = TS.spanEnd isDigit t
-  in case TR.decimal (TS.toText suffix) of
+  let (prefix, suffix) = spanEnd isDigit t
+  in case TR.decimal suffix of
        Right (n, _) -> (prefix, n)
        Left _       -> (t, 0)
 
-sameDisk :: (ShortText, Partition) -> (ShortText, Partition) -> Bool
+spanEnd :: (Char -> Bool) -> T.Text -> (T.Text, T.Text)
+spanEnd p txt =
+  let rev = T.reverse txt
+      (suf, pre) = T.span p rev
+  in (T.reverse pre, T.reverse suf)
+
+sameDisk :: (T.Text, Partition) -> (T.Text, Partition) -> Bool
 sameDisk (d1, _) (d2, _) = d1 == d2
 
-collect :: [(ShortText, Partition)] -> (ShortText, [Partition])
+collect :: [(T.Text, Partition)] -> (T.Text, [Partition])
 collect xs@((d, _) : _) = (d, map snd xs)
 collect [] = error "collect: empty list"
 
---
+++ Installer/src/Storage/Formatter.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Storage.Formatter
+  ( FS(..)
+  , FormatOptions(..)
+  , FormatResult(..)
+  , safeFormat
+  ) where
+
+import System.Process (readProcessWithExitCode)
+import System.Exit (ExitCode(..))
+import qualified Data.Text.Short as TS
+import qualified Data.Text as T
+import Data.List (isInfixOf)
+import Data.Maybe (mapMaybe)
+
+data FS = Ext4 | Btrfs | F2fs | Xfs | Fat32
+  deriving (Show, Eq)
+
+data FormatOptions = FormatOptions
+  { dryRun       :: !Bool
+  , extraArgs    :: ![TS.ShortText]
+  , requireToken :: !TS.ShortText
+  } deriving (Show, Eq)
+
+data FormatResult
+  = FRDryRun !TS.ShortText
+  | FRSuccess !TS.ShortText !TS.ShortText
+  | FRFailure !TS.ShortText
+  deriving (Show, Eq)
+
+safeFormat :: TS.ShortText -> FS -> FormatOptions -> IO FormatResult
+safeFormat device fs opts = do
+  existsOk <- isBlockDevice device
+  if not existsOk
+    then return $ FRFailure $ "Device not found: " <> device
+    else do
+      mounted <- findMounts device
+      if not (null mounted)
+        then return $ FRFailure $ "Device is mounted: " <> TS.fromText (T.pack (unlines mounted))
+        else if requireToken opts /= device
+          then return $ FRFailure $ "RequireToken is missing: " <> device
+          else do
+            let cmd = buildMkfsCommand device fs (extraArgs opts)
+            if dryRun opts
+              then return $ FRDryRun cmd
+              else executeCmd cmd
+
+isBlockDevice :: TS.ShortText -> IO Bool
+isBlockDevice dev = do
+  (ec, _, _) <- readProcessWithExitCode "bash" ["-c", "test -b " ++ TS.unpack dev] ""
+  pure (ec == ExitSuccess)
+
+findMounts :: TS.ShortText -> IO [String]
+findMounts dev = do
+  content <- readFile "/proc/mounts"
+  let ls = lines content
+  pure $ mapMaybe (extractMount dev) ls
+
+extractMount :: TS.ShortText -> String -> Maybe String
+extractMount dev line =
+  if TS.unpack dev `isInfixOf` line
+     then case words line of
+            (mnt:_) -> Just mnt
+            _       -> Nothing
+     else Nothing
+
+buildMkfsCommand :: TS.ShortText -> FS -> [TS.ShortText] -> TS.ShortText
+buildMkfsCommand dev fs extras =
+  let base = case fs of
+        Ext4  -> "mkfs.ext4 -F"
+        Btrfs -> "mkfs.btrfs -f"
+        F2fs  -> "mkfs.f2fs -f"
+        Xfs   -> "mkfs.xfs -f"
+        Fat32 -> "mkfs.fat -F32"
+      extrasPart = TS.intercalate " " extras
+  in TS.fromText $ T.pack $
+       unwords ["bash", "-c", shellEscape (unwords [TS.unpack base, TS.unpack extrasPart, TS.unpack dev])]
+
+executeCmd :: TS.ShortText -> IO FormatResult
+executeCmd bashCmd = do
+  (ec, out, err) <- readProcessWithExitCode "bash" ["-c", TS.unpack bashCmd] ""
+  case ec of
+    ExitSuccess   -> pure $ FRSuccess (TS.fromText (T.pack out)) (TS.fromText (T.pack err))
+    ExitFailure _ -> pure $ FRFailure (TS.fromText (T.pack err))
+
+shellEscape :: String -> String
+shellEscape s = "'" ++ concatMap escapeChar s ++ "'"
+  where
+    escapeChar '\'' = "'\"'\"'"
+    escapeChar c    = [c]
