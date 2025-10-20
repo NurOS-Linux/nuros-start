416f617906641ed7bec4667a197b537e761059fe
Ruzen42 <123930771+ruzen42@users.noreply.github.com>
2025-10-20 12:34:36 +0600

    Update ManageUser.hs

--- Installer/src/User/ManageUser.hs
+++ Installer/src/User/ManageUser.hs
@@ -1,36 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 -- NurOS Ruzen42 2025
-module User.ManageUser where
+module User.ManageUser
+  ( User(..)
+  , URResult(..)
+  , createUser
+  ) where
 
 import GHC.Generics (Generic)
-import Data.Text.Short (ShortText)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Logger
 import qualified Data.Text.Short as ST
-import System.Process (callProcess)
+import qualified Data.Text as T
+import System.Process (readProcessWithExitCode)
+import System.Exit (ExitCode(..))
 
 data User = User
-  { username :: ShortText 
-  , password :: ShortText  
-  , uid      :: Int         
-  , gid      :: Int      
-  , comment  :: ShortText 
-  , homeDir  :: ShortText  
-  , shell    :: ShortText   
+  { username :: !ST.ShortText
+  , password :: !ST.ShortText
+  , uid      :: !Int
+  , gid      :: !Int
+  , comment  :: !ST.ShortText
+  , homeDir  :: !ST.ShortText
+  , shell    :: !ST.ShortText
   } deriving (Show, Eq, Generic)
 
-createUser :: User -> IO ()
-createUser user = do
-  let uname = ST.unpack (username user)
-      uidStr = show (uid user)
-      gidStr = show (gid user)
-      gecos  = ST.unpack (comment user)
-      home   = ST.unpack (homeDir user)
-      sh     = ST.unpack (shell user)
-  callProcess "useradd"
-    [ "-u", uidStr
-    , "-g", gidStr
-    , "-c", gecos
-    , "-d", home
-    , "-s", sh
-    , uname
-    ]
+data URResult
+  = URSuccess !ST.ShortText
+  | URFailure !ST.ShortText
+  | URDryRun  !ST.ShortText
+  deriving (Show, Eq)
+
+createUser :: (MonadLogger m, MonadIO m) => Bool -> User -> m URResult
+createUser dryRun User{username, uid, gid, comment, homeDir, shell} = do
+  let uname  = ST.toText username
+      uidStr = T.pack (show uid)
+      gidStr = T.pack (show gid)
+      gecos  = ST.toText comment
+      home   = ST.toText homeDir
+      sh     = ST.toText shell
+
+      cmdArgs =
+        [ "-u", uidStr
+        , "-g", gidStr
+        , "-c", gecos
+        , "-d", home
+        , "-s", sh
+        , uname
+        ]
+
+      fullCmd = "useradd " <> T.unwords cmdArgs
+
+  if dryRun
+    then do
+      logDebugN $ "Dry-run: " <> fullCmd
+      pure $ URDryRun (ST.fromText fullCmd)
+    else do
+      logInfoN $ "Creating user: " <> uname
+      (ec, _out, err) <- liftIO $ readProcessWithExitCode "useradd" (map T.unpack cmdArgs) ""
+      case ec of
+        ExitSuccess -> do
+          logInfoN $ "User created successfully: " <> uname
+          pure $ URSuccess username
+        ExitFailure _ -> do
+          logErrorN $ "Failed to create user: " <> uname <> " (" <> T.pack err <> ")"
+          pure $ URFailure (ST.fromText (T.pack err))
